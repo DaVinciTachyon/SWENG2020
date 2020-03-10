1774386f135ca18e4ae3b6960ba003ddf47e6609
lolal <nolanl13@tcd.ie>
2020-03-10 13:49:57 +0000

    adding in my stuff to the github

---
+++ EiSHT/lib/goals/detailsPage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+  final heroTag;
+  final goalName;
+
+  DetailsPage({this.heroTag, this.goalName});
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF7A9BEE),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Center(
+          child: Text(
+            'Track Goals',
+            style: TextStyle(
+                fontFamily: 'Monserrat', fontSize: 18.0, color: Colors.white),
+          ),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {},
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height - 82.0,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.transparent),
+              Positioned(
+                top: 75.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(45.0),
+                      topRight: Radius.circular(45.0),
+                    ),
+                    color: Colors.white,
+                  ),
+                  height: MediaQuery.of(context).size.height - 100.0,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
+              Positioned(
+                top: 30.0,
+                left: (MediaQuery.of(context).size.width / 2) - 100,
+                child: Hero(
+                  tag: widget.heroTag,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(widget.heroTag),
+                        fit: BoxFit.cover
+                      ),
+                    ),
+                    height: 200.0,
+                    width:200.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
