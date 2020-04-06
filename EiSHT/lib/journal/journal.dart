086dade170e1c74a353857a47d056282dee3e973
hamza-mughees <mugheesh@tcd.ie>
2020-04-06 16:31:11 +0100

    Added correct indentation

--- EiSHT/lib/journal/journal.dart
+++ EiSHT/lib/journal/journal.dart
@@ -1,6 +1,7 @@
 import '../repository/model.dart';
 import '../repository/repo_service_journal.dart';
 import 'package:flutter/material.dart';
+
 class JournalPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => JournalPageState();
@@ -13,18 +14,13 @@ class JournalPageState extends State<JournalPage> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
         iconTheme: IconThemeData(
-
           color: Theme.of(context).buttonColor, //change your color here
         ),
         centerTitle: true,
         elevation: 0.0,
-        backgroundColor: Theme
-            .of(context)
-            .primaryColor,
-
+        backgroundColor: Theme.of(context).primaryColor,
         title: Text(
           "Journal Page",
-
           style: TextStyle(
             color: Colors.white,
             fontFamily: 'Montserrat',
@@ -43,7 +39,6 @@ class JournalScreen extends StatefulWidget {
 }
 
 class JournalScreenState extends State<JournalScreen> {
-
   final TextEditingController _textController = new TextEditingController();
   Future<List<Journal>> _entries;
   String body;
@@ -52,32 +47,28 @@ class JournalScreenState extends State<JournalScreen> {
   int count;
   String date;
   String content;
- @override
- initState()  {
+  @override
+  initState() {
     super.initState();
     _entries = RepositoryServiceJournal.getAllJournals();
-    setState(() {
-
-    });
+    setState(() {});
   } // .. To hold all journal entries
 
   void readData() async {
     final journal = await RepositoryServiceJournal.getJournal(id);
     content = (journal.body);
   }
+
   void createJournal() async {
-
-
-       count = await RepositoryServiceJournal.journalsCount();
-      final journal = Journal(count, body, date, time, false);
-      await RepositoryServiceJournal.addJournal(journal);
-      setState(() {
-        id = journal.id;
-        _entries = RepositoryServiceJournal.getAllJournals();
-
-      });
-
+    count = await RepositoryServiceJournal.journalsCount();
+    final journal = Journal(count, body, date, time, false);
+    await RepositoryServiceJournal.addJournal(journal);
+    setState(() {
+      id = journal.id;
+      _entries = RepositoryServiceJournal.getAllJournals();
+    });
   }
+
   updateJournal(Journal journal) async {
     journal.body = 'please 🤫';
     await RepositoryServiceJournal.updateJournal(journal);
@@ -92,9 +83,7 @@ class JournalScreenState extends State<JournalScreen> {
       id = null;
       _entries = RepositoryServiceJournal.getAllJournals();
     });
-
   }
-
 
   void _handleSubmission(String text) {
     _textController.clear();
@@ -107,7 +96,7 @@ class JournalScreenState extends State<JournalScreen> {
       // if u want to remove seconds from the time of entry,
       // change the second paramter of "time" from 19 to 16,
       // to add seconds back, change from 16 to 19 :).
-      date = day+"/"+month;
+      date = day + "/" + month;
       body = text;
       time = times;
 
@@ -118,7 +107,7 @@ class JournalScreenState extends State<JournalScreen> {
   Widget _textComposer() {
     // .. user input
     return Container(
-      color:Theme.of(context).buttonColor,
+      color: Theme.of(context).buttonColor,
       margin: EdgeInsets.symmetric(
         horizontal: 10.0,
         vertical: 10.0,
@@ -129,24 +118,19 @@ class JournalScreenState extends State<JournalScreen> {
             child: TextField(
               // .. for journal entries
               decoration: InputDecoration(
-
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(30.0),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(30.0),
                   borderSide: BorderSide(
-                    color: Theme
-                        .of(context)
-                        .primaryColor,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
                 hintText: "Make new entry...",
                 prefixIcon: Icon(
                   Icons.note,
-                  color: Theme
-                      .of(context)
-                      .primaryColor,
+                  color: Theme.of(context).primaryColor,
                 ),
               ),
               controller: _textController,
@@ -159,9 +143,7 @@ class JournalScreenState extends State<JournalScreen> {
             child: IconButton(
               // .. save entry
               icon: Icon(Icons.class_),
-              color: Theme
-                  .of(context)
-                  .primaryColor,
+              color: Theme.of(context).primaryColor,
               onPressed: () => _handleSubmission(_textController.text),
             ),
           ),
@@ -178,21 +160,17 @@ class JournalScreenState extends State<JournalScreen> {
           if (snapshot.hasError) {
             print("has error");
             return new Text('Error: ${snapshot.error}');
-          }
-          else {
+          } else {
             return createListView(context, snapshot);
           }
-        }
-    );
+        });
     return Column(
       children: <Widget>[
         Flexible(
           child: futureBuilders,
-
         ),
         Divider(height: 5.0),
         Container(
-
           decoration: BoxDecoration(
             color: Theme.of(context).buttonColor,
           ),
@@ -201,55 +179,55 @@ class JournalScreenState extends State<JournalScreen> {
       ],
     );
   }
-  createListView(BuildContext context, AsyncSnapshot snapshot){
+
+  createListView(BuildContext context, AsyncSnapshot snapshot) {
     List<Journal> _entries = snapshot.data;
-      return new ListView.builder(
+    return new ListView.builder(
+      itemCount: _entries.length,
+      itemBuilder: (context, int index) {
+        print("count is " + _entries.length.toString());
+        id = index;
+        return new Dismissible(
+          key: UniqueKey(),
+          onDismissed: (direction) {
+            // .. delete entry
+            Journal t = _entries[index];
+            deleteJournal(t);
+            Scaffold.of(context).showSnackBar(
+              SnackBar(
+                // .. delete message
 
-        itemCount: _entries.length,
-        itemBuilder: (context, int index) {
-          print("count is " + _entries.length.toString());
-          id = index;
-          return new Dismissible(
-
-            key: UniqueKey(),
-            onDismissed: (direction) {
-              // .. delete entry
-              Journal t =  _entries[index];
-              deleteJournal(t);
-              Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  // .. delete message
-
-                  content: Text("ENTRY REMOVED"),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  backgroundColor: Color.fromRGBO(0, 0, 0, 0.65),
-                  elevation: 0,
-                  behavior: SnackBarBehavior.floating,
-                  // action: SnackBarAction(
-                  //   label: "UNDO",
-                  //   onPressed: () {
-                  //     // .. undo function needs to be implemented
-                  //   },
-                  // ),
+                content: Text("ENTRY REMOVED"),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-              );
-            },
+                backgroundColor: Color.fromRGBO(0, 0, 0, 0.65),
+                elevation: 0,
+                behavior: SnackBarBehavior.floating,
+                // action: SnackBarAction(
+                //   label: "UNDO",
+                //   onPressed: () {
+                //     // .. undo function needs to be implemented
+                //   },
+                // ),
+              ),
+            );
+          },
           child: _entries[index],
-          );
-        },
-      );
-    }
+        );
+      },
+    );
+  }
 }
-class Journal extends StatelessWidget{
+
+class Journal extends StatelessWidget {
   int id;
   String body;
   String date;
   String time;
   bool isDeleted;
 
-  Journal(this.id, this.body, this.date,this.time, this.isDeleted);
+  Journal(this.id, this.body, this.date, this.time, this.isDeleted);
 
   Journal.fromJson(Map<String, dynamic> json) {
     this.id = json[DatabaseCreator.id];
@@ -259,35 +237,28 @@ class Journal extends StatelessWidget{
     this.isDeleted = json[DatabaseCreator.isDeleted] == 1;
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Container(
-      color:Theme.of(context).scaffoldBackgroundColor,
+      color: Theme.of(context).scaffoldBackgroundColor,
       margin: EdgeInsets.only(
         left: 15.0,
-        right: 15.0,
         top: 15.0,
       ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-
             // .. date and time box
             padding: EdgeInsets.all(7.0),
             decoration: BoxDecoration(
-              color: Theme
-                  .of(context)
-                  .buttonColor,
+              color: Theme.of(context).buttonColor,
               border: Border.all(
-                color: Theme
-                    .of(context)
-                      .buttonColor,
+                color: Theme.of(context).buttonColor,
               ),
               borderRadius: BorderRadius.circular(7),
               boxShadow: [
                 BoxShadow(
-
                   color: Colors.black,
                   blurRadius: 1.0,
                   offset: Offset(.2, .2),
@@ -309,13 +280,9 @@ class Journal extends StatelessWidget{
             padding: EdgeInsets.all(15.0),
             alignment: Alignment.centerLeft,
             decoration: BoxDecoration(
-              color: Theme
-                  .of(context)
-                  .buttonColor,
+              color: Theme.of(context).buttonColor,
               border: Border.all(
-                color: Theme
-                    .of(context)
-                      .buttonColor,
+                color: Theme.of(context).buttonColor,
               ),
               borderRadius: BorderRadius.circular(7),
               boxShadow: [
@@ -346,5 +313,5 @@ make entries look better                                  (✔)
 swipe to delete an entry                                  (✔)
 add undo deletion functionality                           ()
 note saying swipe to delete after one entry               ()
-keep entries when exited the app                          ()
+keep entries when exited the app                          (✔)
 */
