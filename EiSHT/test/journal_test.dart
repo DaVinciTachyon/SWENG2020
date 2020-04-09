fadcc40990161cc7437bbe2a61b0a352e5cdc6d3
David <davidhooban@hotmail.com>
2020-04-09 22:56:42 +0100

    Added widget test to check Journal widget is correctly created. Ran into issues with 'row size' and had to add a 'flexible' widget around each container in the widget. Also, couldnt get functions that interact with DB working, seems not possible with flutter

--- EiSHT/test/journal_test.dart
+++ EiSHT/test/journal_test.dart
@@ -1,73 +1,95 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:goals/journal/journal.dart';
-import 'package:goals/repository/model.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  test("testing class constructor instantiation", () {
+    //create mock object
+    Journal newJournal = Journal(1, "test", "05/01/2020", "15:40", false);
+
+    //check each field correctly initialised
+    expect(1, newJournal.id);
+    expect("test", newJournal.body);
+    expect("05/01/2020", newJournal.date);
+    expect("15:40", newJournal.time);
+    expect(false, newJournal.isDeleted);
+  });
+
+  test("testing JSON conversion", () {
+    //create mock JSON object
+    Map<String, dynamic> toJson() =>
+        {
+          'id': 3,
+          'body': "test",
+          'date': "05/01/2020",
+          'time': "15:50",
+          'isDeleted': false,
+        };
+
+    Map<String, dynamic> json = toJson();
+    json.addAll(json);
+    final journal = Journal.fromJson(json);
+
+    //check each field correctly parsed from JSON
+    expect(3, journal.id);
+    expect("test", journal.body);
+    expect("05/01/2020", journal.date);
+    expect("15:50", journal.time);
+    expect(false, journal.isDeleted);
+  });
+
+  testWidgets('testing journal Widget', (WidgetTester tester) async {
+    //checking the widget can be correctly pumped with no errors
+    await tester.pumpWidget(createWidgetForTesting(child: Journal(1, "test", "05/01/2020", "15:40", false)));
+  });
+
+}
+
+Widget createWidgetForTesting({Widget child}){
+  return MaterialApp(
+    home: child,
+  );
+}
 //  testWidgets('Journal entry tests', (WidgetTester tester) async {
 //  });
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  //Group all related unit tests for journal entries
-  group("Journal Class testing", () {
-      test("testing class constructor instantiation", ()
-      {
-        //create mock object
-        Journal newJournal = Journal(1, "test", "05/01/2020", "15:40", false);
-
-        //check each field correctly initialised
-        expect(1, newJournal.id);
-        expect("test", newJournal.body);
-        expect("05/01/2020", newJournal.date);
-        expect("15:40", newJournal.time);
-        expect(false, newJournal.isDeleted);
-      });
-
-      test("testing JSON conversion", ()
-      {
-        //create mock JSON object
-        Map<String, dynamic> toJson() =>
-            {
-              'id': 3,
-              'body': "test",
-              'date': "05/01/2020",
-              'time': "15:50",
-              'isDeleted': false,
-            };
-
-        Map<String, dynamic> json = toJson();
-        json.addAll(json);
-        final journal = Journal.fromJson(json);
-
-        //check each field correctly parsed from JSON
-        expect(3, journal.id);
-        expect("test", journal.body);
-        expect("05/01/2020", journal.date);
-        expect("15:50", journal.time);
-        expect(false, journal.isDeleted);
-      });
 
 
-     /*
-     * testWidgets('title',(WidgetTester tester) async {
-     *  await test.pumpWidget(   < the widget we want to test > );
-     *  //expect(find.text(Text, 'X'), findsOneWidget); <-- check for widget by text
-     * }
-     */
-  });
+//  //Group all related unit tests for journal entries
+//  group("Journal Class testing", ()
+//  {}
+
+////
+//    group("JournalScreenState Class testing", () {
+//      sqfliteFfiInit();
+////    test('simple sqflite example', () async {
+////      var db = await databaseFactoryFfi.openDatabase('journal_db');
+////      expect(await db.getVersion(), 0);
+////      await db.close();
+////    });
+
+//      test("testing handleSubmission", () async {
+////      var db = await databaseFactoryFfi.openDatabase('journal_db');
+////      expect(await db.getVersion(), 0);
+////      await db.execute('''
+////        CREATE TABLE journal (
+////            id INTEGER PRIMARY KEY,
+////            body TEXT,
+////            date TEXT,
+////            time TEXT,
+////            isDeleted BIT NOT NULL
+////        )
+////        ''');
 //
-//  group("JournalScreenState Class testing", () {
-//    test("testing handleSubmission", () async {
-//      await DatabaseCreator().initDatabase();
-//      print("initialized");
-//      JournalScreenState journalScreenState = JournalScreenState();
-//      journalScreenState.handleSubmission("test input"); //note: this required removing _ from handleSubmission
-//
-//    });
+//        JournalScreenState journalScreenState = JournalScreenState();
+//        journalScreenState.handleSubmission(
+//            "test input"); //note: this required removing _ from handleSubmission
+//      });
 //    test("testing readData", () {
 //        JournalScreenState journalScreenState = JournalScreenState();
 //        journalScreenState.readData();
 //        expect(false, journalScreenState.content.isEmpty);
 //      });
-//  });
-}
+//    });
